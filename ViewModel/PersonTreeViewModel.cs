09e8ba2563f32e4b96ec042ce67b813407453a3f
Aleksejs Uljanovs <aleksejs.uljanovs@csb.gov.lv>
2018-12-05 17:23:11 +0200

    PersonTreeView started

---
+++ ViewModel/PersonTreeViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GRAppLib;
+using GRAppLib.DB;
+using zLib;
+
+namespace GenealogyResearchApp.ViewModel {
+	public class PersonTreeViewModel : Notifiable{
+		private GRDBCont db;
+		public PersonTreeViewModel() {
+			RootPair = new PersonPairNode() {
+				X = 50,
+				Y = 150,
+				Male = new GRAppLib.DB.Person() { FirstName_ = "Alpha", BirthDate = DateTime.Now},
+				Female = new GRAppLib.DB.Person() { FirstName_ = "Beta", BirthDate = DateTime.Now}
+			};
+		}
+
+		public PersonTreeViewModel(GRDBCont DB) {
+			db = DB;
+			
+		}
+		
+		private PersonPairNode rootPair;
+
+		public PersonPairNode RootPair { get { return rootPair; } set {
+				Pairs.Clear();
+				rootPair = value;
+				Pairs.Add(RootPair);
+				RootPair.ExpandBranch = new UVMCommand(p=> {
+					if(RootPair.Male != null) {
+						Pairs.Add(new PersonPairNode() {
+							Male = 
+						})
+					}
+				});
+				RaisePropertyChanged("RootPair");
+			}
+		}
+		void InitPair(PersonPairNode pp) {
+			pp.ExpandBranch = new UVMCommand(p => {
+				if (pp.Male != null) {
+					pp.MaleParentsPair = new PersonPairNode() {
+						X = pp.X + 200,
+						Y = pp.Y - 50,
+						Male = pp.Male.Father,
+						Female = pp.Male.Mother
+					};
+					Pairs.Add(pp.MaleParentsPair);
+					InitPair(pp.MaleParentsPair);
+				}
+				if (pp.Female != null) {
+					pp.FemaleParentsPair = new PersonPairNode() {
+						X = pp.X + 200,
+						Y = pp.Y + 50,
+						Male = pp.Female.Father,
+						Female = pp.Female.Mother
+					};
+					pairs.Add(pp.FemaleParentsPair);
+					InitPair(pp.FemaleParentsPair);
+				}
+			});
+		}
+
+		private List<PersonPairNode> pairs;
+		public List<PersonPairNode> Pairs { get { return pairs; } set { pairs = value; RaisePropertyChanged("Pairs"); } }
+	}
+}
